b17592707a08bf770678a7d6a09529fa4a3774a6
wangxiaodong <apple_xiaodong5896@163.com>
2019-06-11 09:17:49 +0800

    second commit

--- src/com/dong/HelloWorld.java
+++ src/com/dong/HelloWorld.java
@@ -3,5 +3,6 @@ package com.dong;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello welcome to git");
+        System.out.println("Nice to meet you");
     }
 }
