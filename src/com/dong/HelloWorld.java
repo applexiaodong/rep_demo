2e3671799c363b072ecf9c020208351dc543ad3e
wangxiaodong <apple_xiaodong5896@163.com>
2019-06-11 09:50:22 +0800

    fouth commit

--- src/com/dong/HelloWorld.java
+++ src/com/dong/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello welcome to git");
         System.out.println("Nice to meet you");
+        System.out.println("I like to see you,do you know");
     }
 }
